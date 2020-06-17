9730c8805588e4d60dae5890599559945a0d93cf
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-06-17 12:00:48 -0300

    estilos usando sass

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App/App";
+
+import "./assets/scss/styles.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
