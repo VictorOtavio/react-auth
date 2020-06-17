e8758c1443efb8851188f0f7416dc475af0a21c6
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-06-17 12:28:13 -0300

    react-router

--- web/src/components/Main/Main.js
+++ web/src/components/Main/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Main = function () {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/register">
+          <div>Register</div>
+        </Route>
+        <Route path="/">
+          <div>Login</div>
+          <Link to="/register">Não tenho login</Link>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default Main;
