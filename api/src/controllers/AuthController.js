32b8a058222520149c55d4252478f28d0994bf2e
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-06-18 14:34:58 -0300

    rota, validação e controller

---
+++ api/src/controllers/AuthController.js
@@ -0,0 +1,9 @@
+import db from "../models";
+const User = db.users;
+
+// Verifica as credenciais e faz login do usuário no sistema
+const login = async (req, res) => {};
+
+export default {
+  login
+};
